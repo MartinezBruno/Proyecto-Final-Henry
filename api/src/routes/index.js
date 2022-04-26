94375b3cec824c8bda38c15305d6c9419644e6e6
MartinezBruno <brunomar006@gmail.com>
2022-04-26 19:03:19 -0300

    feat(api/src): routes for cities

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,7 +2,9 @@ const {Router} = require('express');
 const router = Router();
 
 const region = require('./region');
+const ciudad = require('./ciudad');
 
 router.use('/provincias', region);
+router.use('/ciudad', ciudad);
 
 module.exports = router;
