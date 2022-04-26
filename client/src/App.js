f95ce6f4f23cba214031e6ed122d8e3563a789cb
Oscar Rdgz <raadgtooz@gmail.com>
2022-04-26 09:10:44 -0500

    UP BRANCH

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!
         </a>
       </header>
     </div>
