8492b8d87f1e8764441173924952710c0e0fb6db
Oscar Rdgz <raadgtooz@gmail.com>
2022-04-27 08:00:23 -0500

    landing sin registro ni login

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -11,7 +11,7 @@ export default function LandingPage() {
 
        
       <div className={`container`}>
-        <div className={`${styles.searchbarContainer}`}>
+        <div className={`${styles.searchbarContainer}`} style={{marginBottom: '150px'}}>
         
         <img src={logo} alt="logo" />
 
@@ -21,10 +21,42 @@ export default function LandingPage() {
         <Link to='/'> LINK 3</Link>
   
         <div className='text-center'>
-        <button className='btn btn-info font-weight-bold' style={{color: 'white'}}>REGISTRATE</button>
-        <a href="/login" className='link-success'><p style={{margin: '0px', fontSize:'10px', textDecoration: 'underline'}}>¿O ya tienes una cuenta?</p></a>
+        <button className='btn btn-primary' style={{color: 'white', borderRadius: '20px', width: '180px', fontWeight:'bold'}}>REGISTRATE</button>
+        <a href="/login" className='link-success'><p style={{margin: '0px', fontSize:'10px', textDecoration: 'underline'}}>¿Ya estás registrado? Inicia sesión</p></a>
         </div>
         </div>
+
+        <div className="container text-white text-center">
+          <div style={{marginBottom: '150px'}}>
+          <h1>ENLACES PROFESIONALES DE CALIDAD</h1>
+          <h6>OFRECE O CONTRATA SERVICIOS PROFESIONALES</h6>
+          <br />
+          <a class="btn btn-lg btn-success" href="#">
+             <span>COMIENZA AHORA</span> </a>
+          </div>
+          
+        <div className={styles.widgetContainer}>
+
+            <div className={styles.landingWidget}>
+            <i className="fa fa-users fa-2x text-dark"></i>
+            <hr />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </div>
+            <div className={styles.landingWidget}>
+            <i className="fa fa-trophy fa-2x text-dark"></i>
+            <hr />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </div>
+            <div className={styles.landingWidget}>
+            <i className="fa fa-handshake-o fa-2x text-dark"></i>
+           <hr />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </div>
+
+        </div>
+
+        </div>
+
       </div>
       <div className={styles.hero}></div>
 
