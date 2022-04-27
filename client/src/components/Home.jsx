5e893abd2d716e26c5f6ec5ebe8074b22e165c5f
BrunoBeltramone <brunobeltramone@gmail.com>
2022-04-26 22:25:00 -0300

    NavBar Casi-Terminada

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Alert, Button } from "react-bootstrap"
+import { useState } from "react";
 
 export default function Home(){
-    return(
+
+    const [show, setShow] = useState(true);
+
+    return (
         <>
-            <h4> HOME PRINCIPAL</h4>
+          <Alert show={show} variant="success">
+            <Alert.Heading>How's it going?!</Alert.Heading>
+            <p>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+              lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
+              fermentum.
+            </p>
+            <hr />
+            <div className="d-flex justify-content-end">
+              <Button onClick={() => setShow(false)} variant="outline-success">
+                Close me y'all!
+              </Button>
+            </div>
+          </Alert>
+    
+          {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
         </>
     )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,46 +1,31 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css'
+import { Navbar, Container, Nav, Button, Stack } from "react-bootstrap";
+import Logo from "./Logo2_Definitivo.png";
 
 export default function NavBar() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/home">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-      {/* <p>Holaaa</p> */}
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src={Logo}
+              width="250"
+              height="60"
+              className="d-inline-block align-top"
+            />{" "}
+          </Navbar.Brand>
+          <Stack direction="horizontal" gap={3} >
+            <Button variant="primary" active>
+               Login
+            </Button>{" "}
+            <Button variant="secondary" active>
+              Register
+            </Button>
+          </Stack>
+        </Container>
+      </Navbar>
     </>
   );
 }
