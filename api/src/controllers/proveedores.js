ea74ffc098cc6d3bd6b5a01ac2bc60de14914cc4
MartinezBruno <brunomar006@gmail.com>
2022-04-27 17:24:35 -0300

    fix(api/src/controllers): feature create porveedor

--- api/src/controllers/proveedores.js
+++ api/src/controllers/proveedores.js
@@ -1,4 +1,4 @@
-const { Proveedor, Servicio, Ciudad, Provincia, Pais } = require("../db");
+const { Proveedor, Servicio, Ciudad, Provincia, Pais } = require('../db')
 
 const createProv = async (req, res) => {
   const {
@@ -12,43 +12,38 @@ const createProv = async (req, res) => {
     pais,
     provincia,
     ciudad,
-  } = req.body;
+  } = req.body
 
-  let newProv = await Proveedor.create({
+  let newProveedor = await Proveedor.create({
     NOMBRE_APELLIDO_PROVEEDOR: `${nombre} ${apellido}`,
     PASSWORD: password,
     EMAIL: email,
     IMAGEN: imagen,
     FECHA_NACIMIENTO: fecha_nacimiento,
-  });
-
+  })
   let serviciosDisp = await Servicio.findAll({
     where: { NOMBRE_SERVICIO: servicios },
-  });
-  console.log(serviciosDisp);
-  newProv.addServicios(serviciosDisp);
-
-  let paisDisp = await Pais.findAll({
+  })
+  
+  let paisDisp = await Pais.findOne({
     where: { NOMBRE_PAIS: pais },
-  });
-
-  newProv.addPai(paisDisp);
-
-  //   let provinciasDisp = await Provincia.findAll({
-  //     where: { NOMBRE_PROVINCIA: provincia },
-  //   });
-
-  //   newProv.addProvincia(provinciasDisp);
-
-  //   let ciudadesDisp = await Ciudad.findAll({
-  //     where: { NOMBRE_CIUDAD: ciudad },
-  //   });
-
-  //   newProv.addCiudad(ciudadesDisp);
-
-  return res.status(201).send("Proveedor creado");
-};
+  })
+  
+  let provinciasDisp = await Provincia.findOne({
+    where: { NOMBRE_PROVINCIA: provincia },
+  })
+  
+  let ciudadesDisp = await Ciudad.findOne({
+    where: { NOMBRE_CIUDAD: ciudad },
+  })
+  
+  newProveedor.addServicios(serviciosDisp)
+  newProveedor.setPai(paisDisp)
+  newProveedor.setProvincium(provinciasDisp)
+  newProveedor.setCiudad(ciudadesDisp)
+  return res.status(201).send('Proveedor creado')
+}
 
 module.exports = {
   createProv,
-};
+}
