35393cdf7efaeedcca1ef5c8310a6e4aa572e4c2
BrunoBeltramone <brunobeltramone@gmail.com>
2022-04-28 13:16:57 -0300

    Listorti

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -4,61 +4,17 @@ import logo from "./img-logo/Logo2_Definitivo.png";
 import { Link } from "react-router-dom";
 import {Button, Modal} from 'react-bootstrap'
 import Register from "./Register";
+import NavBar from "./Navbar";
 
 export default function LandingPage() {
-
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
 
   return (
       <>
       
     <div className="container-fluid">
-
+    <NavBar/>
        
       <div className={`container`}>
-        <div className={`${styles.searchbarContainer}`} style={{marginBottom: '150px'}}>
-        
-        <img src={logo} alt="logo" />
-
-        <Link to='/home'> HOME</Link>
-        <Link to='/'> ¿QUIENES SOMOS?</Link>
-        <Link to='/'> LINK 2</Link>
-        <Link to='/'> LINK 3</Link>
-  
-        <div className='text-center'>
-        {/* <button className='btn btn-primary' style={{color: 'white', borderRadius: '20px', width: '180px', fontWeight:'bold'}}>REGISTRATE</button> */}
-
-        <Button variant="primary" onClick={handleShow} className='btn btn-primary' style={{color: 'white', borderRadius: '20px', width: '180px', fontWeight:'bold'}}>
-        REGISTRATE
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title> <h5>¡Registrate gratis!</h5></Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Register />
-        </Modal.Body>
-        {/* <Modal.Footer> */}
-          {/* <Button variant="danger" onClick={handleClose}>
-            Cerrar
-          </Button>
-          <Button variant="success" onClick={handleClose}>
-            Registrarse
-          </Button> */}
-        {/* </Modal.Footer> */}
-      </Modal>
-
-
-
-
-        <a href="/login" className='link-success'><p style={{margin: '0px', fontSize:'10px', textDecoration: 'underline'}}>¿Ya estás registrado? Inicia sesión</p></a>
-        </div>
-        </div>
 
         <div className="container text-white text-center">
           <div style={{marginBottom: '150px'}}>
@@ -66,7 +22,7 @@ export default function LandingPage() {
           <h6>OFRECE O CONTRATA SERVICIOS PROFESIONALES</h6>
           <br />
           <br />
-          <a className="btn btn-lg btn-success" href="#">
+          <a class="btn btn-lg btn-success" href="#">
              <span>¡COMIENZA AHORA!</span> </a>
           </div>
           
@@ -75,17 +31,23 @@ export default function LandingPage() {
             <div className={styles.landingWidget}>
             <i className="fa fa-users fa-2x text-dark"></i>
             <hr />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Conectar con proveedores nunca fue mas facil. Contrata tus servicios Favoritos de forma rapida y segura
+            </div>
+            <div className={styles.landingWidget}>
+              {/* Logo de Seguridad */}
+              <i class="fa fa-shield fa-2x" aria-hidden="true"></i>
+            <hr />
+            Protegemos a los proveedores y usuarios ofreciendo un ecosistema de pago Seguro
             </div>
             <div className={styles.landingWidget}>
             <i className="fa fa-trophy fa-2x text-dark"></i>
             <hr />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Recompensamos su Confianza otorgandole beneficios únicos y exclusivos para vos
             </div>
             <div className={styles.landingWidget}>
             <i className="fa fa-handshake-o fa-2x text-dark"></i>
-           <hr />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            <hr />
+            Conviertete en provedor y ayuda a +10.000 usuarios a Resolver sus necesidades
             </div>
 
         </div>
