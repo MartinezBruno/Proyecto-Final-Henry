d8c0a0536bee4549ec9745c8a929bdf3309def2f
Maximiliano Candia <candiamaximiliano.mac@gmail.com>
2022-04-28 22:35:51 +0000

    hosting configuration finished

--- api/src/app.js
+++ api/src/app.js
@@ -22,7 +22,7 @@ server.use(bodyParser.json({limit: '50mb'}));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+   res.header('Access-Control-Allow-Origin', 'http://159.89.82.240/'); // update to match the domain you will make the request from
    res.header('Access-Control-Allow-Credentials', 'true');
    res.header(
       'Access-Control-Allow-Headers',
@@ -43,7 +43,7 @@ server.use((req, res, next) => {
 // server.use('/videogame', routesV)
 // server.use('/', routes);
 
-server.use('/', routes);
+server.use('/api', routes);
 
 
 
