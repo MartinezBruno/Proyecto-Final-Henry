774fbc58d9f6e6a6bc060b1c434c888be75113ee
Oscar Rdgz <raadgtooz@gmail.com>
2022-05-01 04:11:30 -0500

    ProfilePage done

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Register from "./components/Register";
 import About from "./components/About";
 import Profile from "./components/Profile";
 import Footer from "./components/Footer";
+import ProfileDetails from "./components/ProfileDetails";
 
 
 function App() {
@@ -42,6 +43,7 @@ function App() {
                   <Route path="register" element={<Register />} />
                   <Route path="about" element={<About />} />
                   <Route path="profile" element={<Profile />} />
+                  <Route path="profile/:userID" element={<ProfileDetails />} />
                   
 
 
