122fd87ebd945db82626e7847665566506d52820
santiagobojanich <santidgz@hotmail.com>
2022-05-02 10:52:04 -0300

    demo + DELETE

--- api/src/routes/proveedor.js
+++ api/src/routes/proveedor.js
@@ -5,10 +5,12 @@ const {
   createProv,
   getProv,
   getProvByID,
+  deleteServicio_Prov,
 } = require('../controllers/proveedores.js')
 
 router.get('/', getProv)
 router.get('/:id', getProvByID)
 router.post('/', createProv)
+router.delete('/', deleteServicio_Prov)
 
 module.exports = router
