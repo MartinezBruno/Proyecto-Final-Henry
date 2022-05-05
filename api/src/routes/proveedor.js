72b302d911535ecc5ce9efb2928b4df004e12ab0
federoque <roquefede@gmail.com>
2022-05-05 14:42:11 -0300

    fix merge filtro total

--- api/src/routes/proveedor.js
+++ api/src/routes/proveedor.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-<<<<<<< HEAD
 const {
   getProv,
   getProvByID,
@@ -11,9 +10,6 @@ const {
   filtroPorProvincia,
   filtroProveedor,
 } = require('../controllers/proveedores.js')
-=======
-const { getProv, getProvByID, deleteServicio_Prov, addServicio_Prov, filtroPorProfesion, filtroPorProvincia, filtroPorCiudad, filtroPorPais} = require('../controllers/proveedores.js')
->>>>>>> c797b80ac98412fee59323e41922acd5c19764e5
 
 router.get('/', getProv)
 router.get('/filtro', filtroProveedor)
@@ -23,10 +19,4 @@ router.delete('/:servId/:provId', deleteServicio_Prov)
 // router.get('/filter?pais&provincia&ciudad&profesion', queryFilter)
 router.get('/filter/:service', filtroPorProfesion)
 router.get('/filterP/:provincia', filtroPorProvincia)
-<<<<<<< HEAD
-
-=======
-router.get('/filterC/:ciudad', filtroPorCiudad)
-router.get('/filterPA/:pais', filtroPorPais)
->>>>>>> c797b80ac98412fee59323e41922acd5c19764e5
 module.exports = router
