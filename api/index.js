0e027fdf22852f42341df5601c0233c248a14577
santiagobojanich <santidgz@hotmail.com>
2022-05-05 10:25:27 -0300

    Feat(api/src/proveedores): Filtros Profesion y Provincia

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const autofillProveedores = require('./src/routes/automatic-post')
 
 // Syncing all the models at once.
 conn
-  .sync({ force: true })
+  .sync({ force: false})
   .then(() => {
     server.listen(process.env.DB_PORT, () => {
       console.log(`%s listening at ${process.env.DB_PORT}`) // eslint-disable-line no-console
@@ -22,7 +22,7 @@ conn
     ciudadesDb()
     serviciosDb()
     initialRoles()
-    // autofillProveedores()
+    autofillProveedores()
     console.log('tamo ready')
   })
   
--- api/src/routes/proveedor.js
+++ api/src/routes/proveedor.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { getProv, getProvByID, deleteServicio_Prov, addServicio_Prov } = require('../controllers/proveedores.js')
+const { getProv, getProvByID, deleteServicio_Prov, addServicio_Prov, filtroPorProfesion, filtroPorProvincia} = require('../controllers/proveedores.js')
 
 router.get('/', getProv)
 router.get('/:id', getProvByID)
 router.post('/:id', addServicio_Prov)
 router.delete('/:servId/:provId', deleteServicio_Prov)
+// router.get('/filter?pais&provincia&ciudad&profesion', queryFilter)
+ router.get('/filter/:service', filtroPorProfesion)
+ router.get('/filterP/:provincia', filtroPorProvincia)
 
 module.exports = router
