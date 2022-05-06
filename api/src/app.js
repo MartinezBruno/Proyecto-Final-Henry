7130d1555f6c723ecdd390fb58ddda31e403c7df
MartinezBruno <brunomar006@gmail.com>
2022-05-06 16:03:17 -0300

    Method PATCH

--- api/src/app.js
+++ api/src/app.js
@@ -59,11 +59,8 @@ server.use('/post', automatic_post)
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.header('Access-Control-Allow-Credentials', 'true')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Access-Token'
-  )
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Access-Token')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH')
   next()
 })
 
