c0f2839e5909b7abfd7608995b68742c82c83f78
MartinezBruno <brunomar006@gmail.com>
2022-05-08 15:16:11 -0300

    feat(client/src/components/SuccessfulPayment.jsx): post api/usuario/compraSuccess

--- client/src/components/SuccessfulPayment.jsx
+++ client/src/components/SuccessfulPayment.jsx
@@ -1,28 +1,45 @@
-import React from "react";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
-import { clearServices } from "../redux/slices/shoppingCart";
+import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { clearServices, paymentSuccess } from '../redux/slices/shoppingCart'
+import api from '../services/api'
 
 export default function SuccessfulPayment() {
-    // const { services } = useSelector(state => state.shoppingCart)
-    const dispatch = useDispatch()
+  // const { services } = useSelector(state => state.shoppingCart)
+  const dispatch = useDispatch()
 
-    useEffect(() => {
-        localStorage.clear()
-        dispatch(clearServices())
-    },[])
+  const paymentSuccess = async (cart, id) => {
+    let compra = await api.post('/usuario/compraSuccess', { cart, id })
+    return compra
+  }
 
-    return(
-        <>
-        <div >
-            <div style={{textAlign:"center", marginTop:"200px"}}>
-                <h2>El pago se ha realizo de forma correcta<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSffqbjrQvFYKQMEL7UxUWIdKB_EKjvyUNmvzliNJt4GrvDUV9z4rXmDX0qgRxLmeeSjL0&usqp=CAU" style={{borderRadius:"200px", width:"3rem", margin:"20px"}}/></h2>
-                <NavLink to={"/home"}>
-                <button type="button" class="btn btn-success">Volver al Inicio</button>
-                </NavLink>
-            </div>
+  useEffect(() => {
+    let shoppingCart = JSON.parse(localStorage.getItem('cartList'))
+    let user = JSON.parse(sessionStorage.getItem('user'))
+    paymentSuccess(shoppingCart, user.id)
+    localStorage.clear()
+    dispatch(clearServices())
+  }, [dispatch])
+
+  return (
+    <>
+      <div>
+        <div style={{ textAlign: 'center', marginTop: '200px' }}>
+          <h2>
+            El pago se ha realizo de forma correcta
+            <img
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSffqbjrQvFYKQMEL7UxUWIdKB_EKjvyUNmvzliNJt4GrvDUV9z4rXmDX0qgRxLmeeSjL0&usqp=CAU'
+              style={{ borderRadius: '200px', width: '3rem', margin: '20px' }}
+            />
+          </h2>
+          <NavLink to={'/home'}>
+            <button type='button' class='btn btn-success'>
+              Volver al Inicio
+            </button>
+          </NavLink>
         </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
