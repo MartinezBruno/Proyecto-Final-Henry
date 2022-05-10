25efabb34bfce530ee2d481a30c600e8b4841a16
MartinezBruno <brunomar006@gmail.com>
2022-05-10 10:46:02 -0300

    change URL to localhost porque soy boludo y mergee mal

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://weattend.online/api',
+  baseURL: 'http://localhost:3001/api',
   headers: {
     'Content-Type': 'application/json',
   },
