c402d0cee4bb556c84170da1cd9d0460477830a3
Bruno <brunomar006@gmail.com>
2022-05-13 17:22:27 -0300

    delete unnecessary route

--- api/src/routes/eventos.js
+++ api/src/routes/eventos.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const { getEvents, createEvent, getProveedorEvents } = require('../controllers/event')
+const { getEvents,  getProveedorEvents } = require('../controllers/event')
 const { compraVerify } = require('../controllers/compraVerify')
 
 router.post('/', compraVerify)
-// router.post('/', createEvent)
+
 router.get('/', getEvents)
+
 router.get('/:proveedor_id', getProveedorEvents)
 
 module.exports = router
