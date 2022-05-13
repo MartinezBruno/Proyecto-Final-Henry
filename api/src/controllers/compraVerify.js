063e4eec1456fe6676ac20dd3246b9c2d31b7d6c
federoque <roquefede@gmail.com>
2022-05-13 08:09:14 -0300

    previo a optimizacion compraVerify

--- api/src/controllers/compraVerify.js
+++ api/src/controllers/compraVerify.js
@@ -10,7 +10,11 @@ const compraVerify = async (req, res) => {
 
     let usuario = await Usuario.findOne({ where: { id: idUsuario } })
 
+    // console.log(moment(start))
+    // console.log(moment(start).add(0.5, 'h'))
+
     for (let i = 0; i < idProveedor.length; i++) {
+      let proveedorId = idProveedor[i]
       let provServ = await Proveedor_Servicio.findAll({
         where: { ProveedorId: idProveedor[i] },
       })
@@ -23,17 +27,29 @@ const compraVerify = async (req, res) => {
 
       if (compras) {
         for (let i = 0; i < compras.length; i++) {
+          // console.log('ID PROVEEDOR SERIVICIO', compras[i].ProveedorServicioId)
+          // let proveedorServicio = await Proveedor_Servicio.findOne({where: {id: compras[i].ProveedorServicioId}})
+          let proveedor = await Proveedor.findByPk(proveedorId)
           let evento = await Evento.findOne({ where: { id: compras[i].EventoId } })
           if (evento) {
             let startEvent = moment(evento.START)
             let endEvent = moment(evento.END)
             let startUser = moment(start)
             let endUser = moment(end)
-            if (!(startEvent.isBefore(startUser) && endEvent.isBefore(startUser)) || (startEvent.isAfter(startUser) && endEvent.isAfter(endUser))) {
-              return res.status(304).send({ message: 'Ya existe un evento en ese horario' })
+            if (!(startEvent.isBefore(startUser) && endEvent.isSameOrBefore(startUser)) || (startEvent.isAfter(startUser) && endEvent.isSameOrAfter(endUser))) {
+              return res.status(405).send({
+                message:
+                  'Ya existe un evento programado para ' +
+                  proveedor.NOMBRE_APELLIDO_PROVEEDOR +
+                  ' entre los horarios ' +
+                  new Date(startUser) +
+                  ' y ' +
+                  new Date(endUser),
+              })
             }
           }
         }
+
         let compra = await CompraVerify.create()
         const event = await Evento.create({
           START: start,
@@ -41,7 +57,7 @@ const compraVerify = async (req, res) => {
           TITLE: title,
           DURATION: duration,
         })
-        let proveewjbf = await Proveedor_Servicio.findOne({
+        let proveedorServicio = await Proveedor_Servicio.findOne({
           where: {
             ProveedorId: idProveedor[i],
             ServicioId: idServicio[i],
@@ -49,13 +65,13 @@ const compraVerify = async (req, res) => {
         })
         await compra.setEvento(event)
         await compra.setUsuario(usuario)
-        await compra.setProveedor_Servicio(proveewjbf)
+        await compra.setProveedor_Servicio(proveedorServicio)
       }
     }
-    return res.status(200).send({ message: 'Evento creado' })
+    return res.status(200).send({ message: 'Eventos creados' })
   } catch (error) {
     console.error(error)
-    return res.status(400).send({ message: 'Error al crear el evento' })
+    return res.status(400).send({ message: 'Error al crear los evento' })
   }
 }
 
