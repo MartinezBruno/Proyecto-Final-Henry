cd0f4f67e42eedb65558ae2acf7f8142d4e711f5
Bruno <brunomar006@gmail.com>
2022-05-15 18:37:37 -0300

    change route line

--- api/src/routes/proveedor.js
+++ api/src/routes/proveedor.js
@@ -13,9 +13,9 @@ const {
 } = require('../controllers/proveedores.js')
 
 router.get('/', getProv)
+router.get('/filtro', filtroProveedor)
 router.get('/:id', getProvByID)
 router.put('/:id', putProvider)
-router.get('/filtro', filtroProveedor)
 router.post('/:id', addServicio_Prov)
 router.delete('/:servId/:provId', deleteServicio_Prov)
 // router.get('/filter?pais&provincia&ciudad&profesion', queryFilter)
