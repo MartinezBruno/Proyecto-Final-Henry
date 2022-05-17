69feac2996ea898a572f920e3f7e52f60c9bab77
Bruno <brunomar006@gmail.com>
2022-05-17 17:57:02 -0300

    Deshago paymentProvider

--- api/src/routes/checkout.js
+++ api/src/routes/checkout.js
@@ -2,10 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const PaymentController = require('../controllers/paymentController')
-const { PaymentService, PaymentProvider } = require('../services/paymentService')
+const PaymentService = require('../services/paymentService')
 
 const PaymentInstance = new PaymentController(new PaymentService())
-const PaymentInstance2 = new PaymentController(new PaymentProvider())
 
 router.get('/', (req, res) => {
   return res.json({
@@ -17,8 +16,5 @@ router.post('/payment', (req, res, next) => {
   PaymentInstance.getPaymentLink(req, res, next)
 })
 
-router.post('/payment/provider', (req, res, next) => {
-  PaymentInstance2.getPaymentLink(req, res, next)
-})
 
 module.exports = router
