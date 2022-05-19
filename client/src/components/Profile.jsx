edf123f35b3364c9c7e8f8d9765da300ddd8362d
MartinezBruno <brunomar006@gmail.com>
2022-05-18 23:17:00 -0300

    fixes

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import ProfileProveedor from './ProfileProvider/Profile'
 import ProfileUser from './ProfileUser/Profile'
-import Banned from '../Banned'
+import Banned from './Banned'
 
 export default function Profile() {
-  
   const { user } = useSelector((state) => state.auth)
   let role = user.Role
   if (user) {
